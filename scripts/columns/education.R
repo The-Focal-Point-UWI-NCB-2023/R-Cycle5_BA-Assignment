0317cb4ba5ec424888745143c497b02802c43166
Over-Leveraged <akiel.walsh@mymona.uwi.edu>
2023-07-01 22:01:17 -0500

    fixing Education

--- scripts/columns/education.R
+++ scripts/columns/education.R
@@ -1,1 +1,32 @@
-print(1)
+df <- read.csv(file = file.choose(),stringsAsFactors = TRUE)
+library(dplyr)
+
+#Viewing the number of unique values within the column (Should be 3 (Primary, Secondary, Tertiary))
+length(unique(df$education)) #= 4
+
+#Counting Empty Spaces 
+sum(df$education == "") # 47 missing values
+
+#Counting Na values
+sum(is.na(df$education)) # No missing values
+
+attach(df)
+
+avg_balance <- df %>%
+  filter(education != "") %>%
+  group_by(education) %>%
+  summarize(avg_balance = mean(balance, na.rm = TRUE))
+
+
+#Still working on this
+#df <- df %>%
+  #mutate(education = if_else(education == "", 
+                             #avg_balance[which.min(abs(avg.balance$avg.balance - balance))], 
+                            # education))
+
+
+
+
+
+
+
--- scripts/columns/housing.R
+++ scripts/columns/housing.R
@@ -1,1 +1,14 @@
-print(1)
+df <- read.csv(file = file.choose(),stringsAsFactors = TRUE)
+
+#Viewing the number of unique values within the column (Should be 2 (Yes and NO))
+length(unique(df$housing)) #= 3
+
+#Counting Empty Spaces 
+sum(df$housing == "")
+
+#Counting Na values
+sum(is.na(df$housing))
+
+#Cleaning Empty Spaces by deletion as there is no other option to infer the values
+df <- df[df$housing != "",]
+
