f6d0b3c936645eab443e5694708c1615ea7ee8f2
gabster_tech <gabbzbr@outlook.com>
2023-07-02 17:37:27 -0500

    add for 2a and b

--- scripts/analytics/a_jobsDeposits.R
+++ scripts/analytics/a_jobsDeposits.R
@@ -1,1 +1,27 @@
-print(1)
+library(ggplot2)
+
+avg.job.deposit <- aggregate(deposit ~ job, data, mean, na.rm = TRUE)
+print(avg.job.deposit)
+
+# Plotting the average deposits
+ggplot(avg.job.deposit, aes(x = job, y = deposit)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  xlab("Job") +
+  ylab("Average Deposit") +
+  ggtitle("Average Deposits by Job Category")
+
+#getting the minimum deposit value
+min_deposit <- min(avg.job.deposit$deposit)
+print(min_deposit)
+
+#find the job associated with minimum deposit 
+job_min_deposit <- avg.job.deposit$job[avg.job.deposit$deposit == min_deposit]  
+print(job_min_deposit)
+
+#getting the maximum deposit value
+max_deposit <- max(avg.job.deposit$deposit)
+print(max_deposit)
+
+#find the job associated with minimum deposit 
+job_max_deposit <- avg.job.deposit$job[avg.job.deposit$deposit == max_deposit]
+print(job_max_deposit)
