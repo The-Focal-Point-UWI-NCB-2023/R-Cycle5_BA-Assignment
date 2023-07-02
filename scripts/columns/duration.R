90e8eaed67729071026c124b78ae4be9c4769536
gabster_tech <gabbzbr@outlook.com>
2023-07-01 23:13:06 -0500

    cleaning for duration and months column

--- scripts/columns/duration.R
+++ scripts/columns/duration.R
@@ -1,1 +1,12 @@
-print(1)
+install.packages('lubridate')
+library(lubridate)
+
+str(data)
+hist(data$duration)
+sum(is.na(data$duration))
+
+#convert from seconds to minutes
+## right now a new column is created but the durations column can easily be replaced doing same
+###just in case there is conflict i just created a new column
+data$minutes <- as.duration(data$duration) / dminutes(1)
+data$minutes <- round(minutes,1)
--- scripts/columns/month.R
+++ scripts/columns/month.R
@@ -1,1 +1,11 @@
-print(1)
+#check for na and null values 
+sum(is.na(data$month))
+sum(is.null(data$month))
+
+#create a vector of valid months
+valid_months <- tolower(month.name)
+valid_months
+
+#check if any months are invalid
+invalid_months <- !(tolower(data$month) %in% valid_months)
+sum(invalid_months)
