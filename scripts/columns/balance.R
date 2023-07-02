243c3a2613115477f8b7a506a178d6bb5e4e8384
J11an <jaysun1w@gmail.com>
2023-07-01 21:51:20 -0500

    Add balance prep, fix deposit prep

--- scripts/columns/balance.R
+++ scripts/columns/balance.R
@@ -1,1 +1,19 @@
-print(1)
+str(data)
+summary(data)
+View(data)
+
+boxplot(data)
+
+avg.job.balance <- aggregate(balance ~ job, data, mean, na.rm = TRUE)
+data$avg_balance <- avg.job.balance$balance[match(data$job, avg.job.balance$job)]
+#Replace balance with average balance based on job if the balance is over the 75% quater, because we don't want to affect more than 10% of the value
+#and we would not be affecting up to 10% when we change outliers using this method
+data$balance <- ifelse(data$balance > 278505, as.integer(data$avg_balance), data$balance)
+data$avg_balance <- NULL
+
+
+View(data)
+
+summary(data)
+
+boxplot(data)
--- scripts/columns/deposit.R
+++ scripts/columns/deposit.R
@@ -11,16 +11,16 @@ boxplot(data$deposit)
 #View (sch2)
 
 #Using average deposit based on job
-#avg.job.deposit <- aggregate(deposit ~ job, data, mean, na.rm = TRUE)
-#data$avg_deposit <- avg.job.deposit$deposit[match(data$job, avg.job.deposit$job)]
-#data$deposit <- ifelse(is.na(data$deposit), data$avg_deposit, data$deposit)
-#data$deposit <- NULL
+avg.job.deposit <- aggregate(deposit ~ job, data, mean, na.rm = TRUE)
+data$avg_deposit <- avg.job.deposit$deposit[match(data$job, avg.job.deposit$job)]
+data$deposit <- ifelse(is.na(data$deposit), data$avg_deposit, data$deposit)
+data$avg_deposit <- NULL
 
 #Using average balance based on job
-avg.job.balance <- aggregate(balance ~ job, data, mean, na.rm = TRUE)
-data$avg_balance <- avg.job.balance$balance[match(data$job, avg.job.deposit$job)]
-data$deposit <- ifelse(is.na(data$deposit), data$avg_balance, data$deposit)
-data$avg_balance <- NULL
+#avg.job.balance <- aggregate(balance ~ job, data, mean, na.rm = TRUE)
+#data$avg_balance <- avg.job.balance$balance[match(data$job, avg.job.deposit$job)]
+#data$deposit <- ifelse(is.na(data$deposit), data$avg_balance, data$deposit)
+#data$avg_balance <- NULL
 
 #After Cleaning
 View(data)
