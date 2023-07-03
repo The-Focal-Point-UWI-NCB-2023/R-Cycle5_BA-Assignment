ebb346abacfa900a60e08df158d2d2f528b08b86
Over-Leveraged <akiel.walsh@mymona.uwi.edu>
2023-07-02 20:03:00 -0500

    Fixed Normalization

--- scripts/columns/Normalization-Balance.R
+++ scripts/columns/Normalization-Balance.R
@@ -1,17 +1,13 @@
-attach(df) ## Attach function Makes it so each column of a dataframe can be reference by name without $
-#print(names(df))
 
-#df<-tempdf
-
-min.balance <- min(balance, na.rm = T) # Setting Min Value
-max.balance <- max(balance, na.rm = T) # Setting Max value
+min.balance <- min(data$balance, na.rm = T) # Setting Min Value
+max.balance <- max(data$balance, na.rm = T) # Setting Max value
 
 new.min <- 1 # Setting new min value for normalized Data to 1
 new.max <- 100 # Setting new max value for normalized Data to 100
 
 
 #Normalization Based on Formula
-df<- df %>% 
+data<- data %>% 
   # Mutate function essentially  alters the content of a column.
   # In this case we Altering the deposit column to updated with min max normalized values 
   mutate(balance = as.integer(
@@ -21,6 +17,6 @@ df<- df %>%
   )
 
 
-boxplot(df$balance)
+boxplot(data$balance)
 
 
--- scripts/columns/Normalization-Deposit.R
+++ scripts/columns/Normalization-Deposit.R
@@ -1,24 +1,17 @@
-attach(df) ## Attach function Makes it so each column of a dataframe can be reference by name without $
-print(names(df))
 
-df<-tempdf
-
-min.deposit <- min(deposit, na.rm = T) # Setting Min Value
-max.deposit <- max(deposit, na.rm = T) # Setting Max value
+min.deposit <- min(data$deposit, na.rm = T) # Setting Min Value
+max.deposit <- max(data$deposit, na.rm = T) # Setting Max value
 
 new.min <- 1 # Setting new min value for normalized Data to 1
 new.max <- 100 # Setting new max value for normalized Data to 100
 
-df$deposit <- as.integer(df$deposit)
+#data$deposit <- as.integer(df$deposit)
 
-df<- df %>% 
+data<- data %>% 
   # Mutate function essentially  alters the content of a column.
   # In this case we Altering the deposit column to updated with min max normalized values 
   mutate(deposit = as.integer(
     (deposit - min.deposit)/(max.deposit - min.deposit ) * (100 - 1) + 1
     #Formula for Min Max = normalized_value = (value - minimum_value) / (maximum_value - minimum_value) * (new_max - new_min) + new_min
-    )
   )
-
-boxplot(df$deposit)
-
+  )
