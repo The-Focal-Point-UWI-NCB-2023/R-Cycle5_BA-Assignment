024760b803de586030e97ce544fd381f5b9b720f
gabster_tech <gabbzbr@outlook.com>
2023-07-04 21:10:37 -0500

    add packages to main

--- scripts/columns/month.R
+++ scripts/columns/month.R
@@ -4,16 +4,20 @@ month_na_sum <- sum(is.na(data$month))
 month_null_sum <- sum(is.null(data$month))
 
 #create a list of valid months
-valid_months <- tolower(month.name)
+valid_months <- factor(c(tolower(month.name)))
 
 #check if any months are invalid
 invalid_months <- !(tolower(data$month) %in% valid_months)
 num_of_invalid <- sum(invalid_months)
 
+
 sum_deposits_per_month <- aggregate(deposit ~ month, data, sum)
 
-ggplot(sum_deposits_per_month, aes(x = month, y = deposit)) +
+ggplot(sum_deposits_per_month, aes(x = month, y = deposit, label=deposit)) +
   geom_bar(stat = "identity", fill = "blue") +
+  geom_bar(data = subset(sum_deposits_per_month, deposit == max(deposit)), fill = "red", stat = "identity")+
+  geom_text(size = 3, position = position_stack(vjust=1.02)) 
   xlab("Months") +
   ylab("Amount of Deposits") +
-  ggtitle("Amount of Deposits per month")
+  ggtitle("Amount of Deposits per month") +
+  theme(plot.title = element_text(hjust = 0.5))
