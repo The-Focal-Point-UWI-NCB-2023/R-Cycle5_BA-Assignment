da87b68d2bdad91bb09c705bcbbb4e52cee19776
gabster_tech <gabbzbr@outlook.com>
2023-07-05 17:32:45 -0500

    made edits

--- scripts/analytics/a_jobsDeposits.R
+++ scripts/analytics/a_jobsDeposits.R
@@ -1,10 +1,7 @@
 library(ggplot2)
 
 avg_job_deposit <- aggregate(deposit ~ job, data, mean, na.rm = TRUE) 
-
-
-# Plotting the average deposits
-avg_job_deposit <- aggregate(deposit ~ job, data, mean, na.rm = TRUE) 
+avg_job_deposit$deposit <- round(avg_job_deposit$deposit,2)
 
 # Plotting the average deposits
 ggplot(avg_job_deposit, aes(x = job, y = deposit,label=deposit)) +
@@ -14,7 +11,8 @@ ggplot(avg_job_deposit, aes(x = job, y = deposit,label=deposit)) +
   geom_text(size = 3, position = position_stack(vjust=1.09)) +
   xlab("Job") +
   ylab("Average Deposit") +
-  ggtitle("Average Deposits by Job Category")
+  ggtitle("Average Deposits by Job Category")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 #getting the minimum deposit value
 min_deposit <- min(avg_job_deposit$deposit)
