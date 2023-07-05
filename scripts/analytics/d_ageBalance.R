49a9ba516dd3641a36bf8c2658a5247341390903
Brandon Hall <brandonhall@Brandons-MacBook-Air.local>
2023-07-05 16:33:46 -0500

    all-slides

--- scripts/analytics/d_ageBalance.R
+++ scripts/analytics/d_ageBalance.R
@@ -4,16 +4,16 @@ avg.bal <- c()
 #average the balance
 for (i in 1:length(age.groups)){
   rows <- data[data$age == age.groups[i],]
-  print(age.groups[i])
-  
+  print(rows)
   avg.bal[i] <- round(mean(rows$balance),2)
-  print(avg.bal[i])
 }
 
+c("Age Group:",age.groups[which(avg.bal == max(avg.bal))] )
+c("Max Average Balance: ",max(avg.bal))
 
 # Plot the bar chart 
 barplot(avg.bal, names.arg = age.groups, xlab ="Age Group", 
-        ylim = c(0, max(avg.bal) + 10),
+        ylim = c(0, max(avg.bal) + 20),
         ylab ="Average Balance", col ="lightblue", 
         main ="Average Balance Between Age Groups")
 max(avg.bal)
